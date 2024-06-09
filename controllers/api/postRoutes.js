e37d2ad02e6a099e298c53fef990692b0abffb25
jovanna24 <77941035+jovanna24@users.noreply.github.com>
2024-06-08 20:18:56 -0700

    added all controllers and db schema

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// route to create a new post
+router.post('/', withAuth, async (req, res) => {
+  try {
+    // creates a new post with the data from the request bodu and the user ID from the session
+    const newPost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// route to delet post by ID
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
